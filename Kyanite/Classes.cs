0760ab3aa1f3ee842761911b7345574eba91b88f
Jewels <jewels286@proton.me>
2024-11-14 07:40:30 -0500

    restarting with maui?

--- Kyanite/Classes.cs
+++ Kyanite/Classes.cs
@@ -12,4 +12,22 @@ namespace Kyanite
 		Normal,
 		High
 	}
+	public struct OpenPort
+	{
+		public int Port { get; set; }
+		public string Type { get; set; }
+		public string Service { get; set; }
+		public string Version { get; set; }
+	}
+	public struct Device
+	{
+		public string Name { get; set; }
+		public string IP { get; set; }
+		public string MAC { get; set; }
+		public string Manufacturer { get; set; }
+		public string DeviceType { get; set; }
+		public string OS { get; set; }
+		public string OSVersion { get; set; }
+		public List<OpenPort> OpenPorts { get; set; }
+	}
 }
